58f8c260b62fbff511acb2c7c532d3f1af4ff611
Stephen Wicks <stevewicks1@gmail.com>
2017-07-05 16:38:44 -0400

    return to robots assessment first commit

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ const mustacheExpress = require("mustache-express");
 const path = require("path");
 const app = express();
 const data = require("./data.js");
+var mongo = require("mongodb");
+var assert = require("assert");
+
+var url = 'mongodb://localhost:27017/user';
 
 app.engine('mustache', mustacheExpress());
 app.set('views', path.join(__dirname, 'views'));
@@ -11,12 +15,37 @@ app.set('view engine', 'mustache')
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get("/", function(req, res){
+app.get("/", function(req, res, next){
     res.render("index", data);
 });
-app.get('/profile/:id', function (req, res) {
-    res.render('profile', data);
+
+app.get('get-data', function (req, res) {
+let index = parseInt(req.params.id);
+    res.send(data.users[index-1]);
 });
-app.listen(3000,function(er, res){
+
+app.get('insert', function (req, res) {
+let index = parseInt(req.params.id);
+    res.send(data.users[index-1]);
+});
+
+mongo.connect(url, function(err, db){
+    assert.equal(null, err);
+});
+
+app.get('update', function (req, res) {
+let index = parseInt(req.params.id);
+    res.send(data.users[index-1]);
+});
+
+app.get('delete', function (req, res) {
+let index = parseInt(req.params.id);
+    res.send(data.users[index-1]);
+});
+
+
+app.listen(3000,function(){
     console.log("Server started on Port 3000")
 })
+/*app.get('/profile/:id', function (req, res) {
+    res.render('profile', data);*/
